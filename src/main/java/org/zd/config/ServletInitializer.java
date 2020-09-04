8dbeffeb0f20ef98593633bf98bf3d983f59c0ae
zdezak <zdezarus@gmail.com>
2020-09-04 15:17:22 +0500

    Hello World in SpringWebMVC. Config.java.

---
+++ src/main/java/org/zd/config/ServletInitializer.java
@@ -0,0 +1,20 @@
+package org.zd.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {Config.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
