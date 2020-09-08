e52bb3b42960ae2ef1c96ec79c6fbe87234292bd
zdezak <zdezarus@gmail.com>
2020-09-08 11:28:17 +0500

    Print hello on page with name and surname.

--- src/main/java/org/zd/springMVC/controllers/FirstController.java
+++ src/main/java/org/zd/springMVC/controllers/FirstController.java
@@ -13,8 +13,9 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname){
-        System.out.println("Hello, "+name+" "+surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model){
+        model.addAttribute("message","Hello, "+ name+" "+surname);
         return "first/hello";
     }
     @GetMapping("/goodbye")
