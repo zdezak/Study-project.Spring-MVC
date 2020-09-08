88f866cd1b5d9a0b54690199601f422da308fad3
zdezak <zdezarus@gmail.com>
2020-09-08 10:01:21 +0500

    HTTPServletRequest and @RequestParam.

--- src/main/java/org/zd/springMVC/controllers/FirstController.java
+++ src/main/java/org/zd/springMVC/controllers/FirstController.java
@@ -3,12 +3,17 @@ package org.zd.springMVC.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname){
+        System.out.println("Hello, "+name+" "+surname);
         return "first/hello";
     }
     @GetMapping("/goodbye")
