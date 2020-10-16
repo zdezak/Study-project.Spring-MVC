198e27f523aa427727586cf6959beb58a4bf7bbe
zdezak <zdezarus@gmail.com>
2020-10-16 15:32:47 +0500

    Аннотация @ModelAttribute. HTML Формы (Thymeleaf).

--- src/main/java/org/zd/springMVC/dao/PersonDAO.java
+++ src/main/java/org/zd/springMVC/dao/PersonDAO.java
@@ -8,18 +8,24 @@ import java.util.List;
 @Component
 public class PersonDAO {
     private List<Person> people;
+    private static int PEOPLE_COUNT;
 
     {
         people = new ArrayList<>();
-        people.add(new Person(1,"Tom"));
-        people.add(new Person(2,"Jon"));
-        people.add(new Person(3,"Nick"));
-        people.add(new Person(4,"Mick"));
+        people.add(new Person(++PEOPLE_COUNT,"Tom"));
+        people.add(new Person(++PEOPLE_COUNT,"Jon"));
+        people.add(new Person(++PEOPLE_COUNT,"Nick"));
+        people.add(new Person(++PEOPLE_COUNT,"Mick"));
     }
     public List<Person> index(){
         return people;
     }
     public Person show(int id){
         return people.stream().filter(person->person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
     }
 }
