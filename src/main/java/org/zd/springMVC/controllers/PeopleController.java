189e9e07d917c6c6a18a809ecb3c4a3e9c3c021c
zdezak <zdezarus@gmail.com>
2021-01-08 22:47:11 +0500

    алидация форм. Аннотация @Valid

--- src/main/java/org/zd/springMVC/controllers/PeopleController.java
+++ src/main/java/org/zd/springMVC/controllers/PeopleController.java
@@ -3,9 +3,12 @@ package org.zd.springMVC.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.zd.springMVC.dao.PersonDAO;
 import org.zd.springMVC.models.Person;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -33,7 +36,9 @@ public class PeopleController {
         return "people/new";
     }
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person){
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult){
+        if (bindingResult.hasErrors())
+            return "people/new";
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -43,7 +48,10 @@ public class PeopleController {
         return "people/edit";
     }
     @PostMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id){
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult, @PathVariable("id") int id){
+        if (bindingResult.hasErrors())
+            return "people/edit";
+
         personDAO.update(id,person);
         return "redirect:/people";
     }
