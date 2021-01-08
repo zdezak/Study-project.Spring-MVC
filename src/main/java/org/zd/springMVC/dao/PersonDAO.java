49ca8f97ef25244ebdf00ed7837f778fcda6234a
Evgeni <zdezarus@gmail.com>
2021-01-08 22:50:31 +0500

    Revert "Dev"

--- src/main/java/org/zd/springMVC/dao/PersonDAO.java
+++ src/main/java/org/zd/springMVC/dao/PersonDAO.java
@@ -8,35 +8,18 @@ import java.util.List;
 @Component
 public class PersonDAO {
     private List<Person> people;
-    private static int PEOPLE_COUNT;
 
     {
         people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT,"Tom",16,"tom@mail.com"));
-        people.add(new Person(++PEOPLE_COUNT,"Jon",36,"jon@mail.com"));
-        people.add(new Person(++PEOPLE_COUNT,"Nick",27,"nick@mail.com"));
-        people.add(new Person(++PEOPLE_COUNT,"Mick",52,"mick@mail.com"));
+        people.add(new Person(1,"Tom"));
+        people.add(new Person(2,"Jon"));
+        people.add(new Person(3,"Nick"));
+        people.add(new Person(4,"Mick"));
     }
     public List<Person> index(){
         return people;
     }
     public Person show(int id){
         return people.stream().filter(person->person.getId() == id).findAny().orElse(null);
-    }
-
-    public void save(Person person) {
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
-    }
-
-    public void update(int id, Person updatedPerson) {
-        Person personToBeUpdated = show(id);
-        personToBeUpdated.setName(updatedPerson.getName());
-        personToBeUpdated.setAge(updatedPerson.getAge());
-        personToBeUpdated.setEmail(updatedPerson.getEmail());
-    }
-
-    public void delete(int id) {
-        people.removeIf(p->p.getId()==id);
     }
 }
