deeb49acd0cd93337b66f9a420385ece5b6bf6a2
zdezak <zdezarus@gmail.com>
2021-01-12 14:29:06 +0500

    JdbcTemplate

--- src/main/java/org/zd/springMVC/config/Config.java
+++ src/main/java/org/zd/springMVC/config/Config.java
@@ -5,12 +5,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("org.zd.springMVC")
@@ -48,5 +52,19 @@ public class Config implements WebMvcConfigurer {
         resolver.setTemplateEngine(templateEngine());
         resolver.setCharacterEncoding("UTF-8");
         registry.viewResolver(resolver);
+    }
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/springMVC");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("root");
+
+        return dataSource;
+    }
+    @Bean
+    public JdbcTemplate jdbcTemplate(){
+        return new JdbcTemplate(dataSource());
     }
 }
---
+++ src/main/java/org/zd/springMVC/dao/PersonMapper.java
@@ -0,0 +1,20 @@
+package org.zd.springMVC.dao;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.zd.springMVC.models.Person;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PersonMapper implements RowMapper<Person> {
+    @Override
+    public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Person person = new Person();
+        person.setId(rs.getInt("id"));
+        person.setName(rs.getString("name"));
+        person.setAge(rs.getInt("age"));
+        person.setEmail(rs.getString("email"));
+
+        return person;
+    }
+}
